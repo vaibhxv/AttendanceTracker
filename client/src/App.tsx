7d00d7a553f1efadd4868108682ad99f5c5a2b41
vaibhxv <vaibhavrohaan3@gmail.com>
2025-01-21 14:58:02 +0530

    mx auto change in app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -13,7 +13,7 @@ const App = () => {
     <Router>
       <div className="min-h-screen bg-background">
         <Navbar token={token} setToken={setToken} />
-        <div className="container">
+        <div className="container mx-auto">
           <Routes>
             <Route
               path="/signup"
